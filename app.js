b175382bb761d5f7d6b70a7bae09566ca8b09d59
Aaron <aws.clemens@gmail.com>
2020-09-17 21:18:42 -0600

    doc ready, init, namespace added

---
+++ app.js
@@ -0,0 +1,13 @@
+// namespace
+const app = {};
+
+// init method
+app.init = function() {
+    console.log('initialized');
+    // OUR CODE HERE
+}
+
+// document ready
+$(document).ready(function() {
+    app.init();
+});
